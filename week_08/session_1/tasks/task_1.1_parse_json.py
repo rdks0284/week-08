6a708139f5cdb486720e68260b68499f90778826
Hashim [rdks0284] <rdks0284@leeds.ac.uk>
2025-11-17 12:47:45 +0000

    end of session 1 week 8

---
+++ week_08/session_1/tasks/task_1.1_parse_json.py
@@ -0,0 +1,32 @@
+"""
+Task 1.1: Parsing and Accessing JSON Data
+
+Goal: Learn to parse JSON strings and access nested data.
+"""
+
+import json
+
+# JSON string
+json_string = """
+{
+    "userId": 101,
+    "name": "Alice Smith",
+    "email": "alice.smith@example.co.uk",
+    "isActive": true,
+    "roles": ["admin", "editor"],
+    "address": {
+        "street": "45 Briggate",
+        "city": "Leeds",
+        "postcode": "LS1 6HF"
+    },
+    "lastLogin": "2025-11-21T14:30:00Z"
+}
+"""
+
+# TODO: Parse the JSON string into a Python dictionary using json.loads()
+data = json.loads(json_string)
+
+# TODO: Access values such as name, email, and city from the parsed dictionary
+print(f"Name: {data["name"]}")
+print(f"Email: {data["email"]}")
+print(f"City: {data["address"]["city"]}")
---
+++ week_08/session_1/tasks/task_2.1_fetch_posts.py
@@ -0,0 +1,35 @@
+"""
+Task 2.1: Fetch and Display Posts Using GET Request
+
+Goal: Learn to make GET requests to an API and display the results.
+
+Exercises:
+- Fetch and display all posts (first 5)
+- Fetch a single post by ID
+- Fetch users and display their names
+"""
+
+import httpx, json
+
+# Exercise 1.1: Fetch and display all posts (first 5)
+url = "https://jsonplaceholder.typicode.com/posts"
+# TODO: Send GET request and display first 5 posts
+
+
+# Exercise 1.2: Fetch a single post by ID
+url = "https://jsonplaceholder.typicode.com/posts/1"
+# TODO: Send GET request and display the post details
+
+
+# Exercise 1.3: Fetch users and display their names
+url = "https://jsonplaceholder.typicode.com/users"
+# TODO: Send GET request and display user names
+
+response = httpx.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+
+    print(data[0]['name'])
+else:
+    print(f"Error occured - status code was {response.status_code}")
